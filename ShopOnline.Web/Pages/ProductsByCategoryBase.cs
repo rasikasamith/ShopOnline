7ed864044e3f553e1b05d38950d13241d9ca2a25
rasikasamith <rasikasamith@gmail.com>
2024-02-29 08:04:58 +0530

    Ui Changes

--- ShopOnline.Web/Pages/ProductsByCategoryBase.cs
+++ ShopOnline.Web/Pages/ProductsByCategoryBase.cs
@@ -1,7 +1,41 @@
-﻿namespace ShopOnline.Web.Pages
+﻿using Microsoft.AspNetCore.Components;
+using ShopOnline.Models.DTO;
+using ShopOnline.Web.Services.Contracts;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ShopOnline.Web.Pages
 {
-    public class ProductsByCategoryBase
+    public class ProductsByCategoryBase:ComponentBase
     {
+        [Parameter]
+        public int CategoryId { get; set; }
+        [Inject]
+        public IProductService _iProductService { get; set; }
+        public IEnumerable<ProductDto> Products { get; set; }
+        public string CategoryName { get; set; }
+        public string ErrorMessage { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            try
+            {
+                Products = await _iProductService.GetItemsByCategory(CategoryId);
+                if(Products !=null && Products.Count()>0)
+                {
+                    var productDto = Products.FirstOrDefault(p => p.CategoryId == CategoryId);
+                    if (productDto != null)
+                    {
+                        CategoryName = productDto.CategoryName;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage=ex.Message;
+            }
+        }
+
 
     }
 }
